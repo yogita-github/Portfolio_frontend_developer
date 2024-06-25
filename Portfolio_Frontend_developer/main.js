dad4781ee1fae141a6f0c2505214bbd1bef4e87c
Mahajan Yogita Vinayak <103029645+yogita-github@users.noreply.github.com>
2024-06-25 16:04:45 +0530

    Add files via upload

---
+++ Portfolio_Frontend_developer/main.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".text", {
+  strings: ["Frontend Developer", "Web Developer", "Data Analyst"],
+  typeSpeed: 10,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  alert("Open in web");
+});
